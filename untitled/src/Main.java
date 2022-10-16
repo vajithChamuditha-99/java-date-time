e7f6003e40d4c1d6c6b17a22a14566e1402041d9
Vajith Chamuditha <vajithc@gmail.com>
2022-10-16 15:04:14 +0530

    Zoned date time

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -12,6 +12,14 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
+
+//        final ZonedDateTime input = ZonedDateTime.now();
+//        System.out.println(input);
+//        final ZonedDateTime startOfLastWeek = input.minusWeeks(1).with(DayOfWeek.MONDAY);
+//        System.out.println(startOfLastWeek);
+//        final ZonedDateTime endOfLastWeek = startOfLastWeek.plusDays(6);
+//        System.out.println(endOfLastWeek);
+
         LocalDateTime dt = LocalDateTime.now();
         System.out.println("\nNext Friday: "+dt.with(TemporalAdjusters.next(DayOfWeek.FRIDAY)));
         System.out.println("Previous Friday: "+dt.with(TemporalAdjusters.previous(DayOfWeek.FRIDAY))+"\n");
